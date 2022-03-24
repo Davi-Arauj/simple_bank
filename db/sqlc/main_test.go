f0f618b8f63c3463938701bb15a9fa26fc4e867d
Davi-Arauj <moreiradaavi@gmail.com>
2022-03-24 14:39:41 -0300

    Verify Error

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,22 +6,24 @@ import (
 	"os"
 	"testing"
 
-	_"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
-var testQueries *Queries
 
-const(
+const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
-func TestMain(m *testing.M){
-	conn, err := sql.Open(dbDriver,dbSource)
-	if err != nil{
-		log.Fatal("cannot connect to db:",err)
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	testDB, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/simplebank/simplebank
 go 1.13
 
 require (
-	github.com/kyleconroy/sqlc v1.12.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/lib/pq v1.10.4
 	github.com/stretchr/testify v1.7.1
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
